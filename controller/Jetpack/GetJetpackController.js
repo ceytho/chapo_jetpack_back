ef748cb88b6026ac8b7c19b820e7f89fc92ee14e
ceytho <kaviane@hotmail.fr>
2019-10-10 10:44:08 +0200

    Show jetpack list in GET (with test draft)

--- controller/Jetpack/GetJetpackController.js
+++ controller/Jetpack/GetJetpackController.js
@@ -1,4 +1,18 @@
+const Jetpack = require("../../src/Entity/Jetpack");
+const uuidv4 = require('uuid/v4');
+const db = require('../../src/Db');
+const JetpackRepository = require('../../src/Repository/JetpackRepository');
+
 module.exports = (req, res) => {
-    var id = req.params.id;
-    res.send('Not implemented');
+    var start_date = req.query.start_date;
+    var end_date = req.query.end_date;
+    
+    if(start_date !== undefined && end_date !== undefined) {
+        res.send('To implement (startdate ' + start_date + ' enddate ' + end_date + ')');
+    } else {
+        const repository = new JetpackRepository(db);
+        jetpacks = repository.getAll();
+        res.header("Access-Control-Allow-Origin", "*");
+        res.status(201).send(jetpacks);
+    }
 };
