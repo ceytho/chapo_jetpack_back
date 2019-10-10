1b45c4563da9364a39a8576a257608e105d3bd7f
ceytho <kaviane@hotmail.fr>
2019-10-10 12:36:56 +0200

    jetpackrepository additionnal fix

--- src/Repository/jetpackrepository.test.js
+++ src/Repository/jetpackrepository.test.js
@@ -1,16 +1,34 @@
 const JetpackRepository = require('./JetpackRepository');
-const Jetpack = require('../Entity/Jetpack');
 
 describe('JetpackCreation', function() {
     const dbMock = {
         get : jest.fn().mockReturnThis(),
         push : jest.fn().mockReturnThis(),
-        write : jest.fn().mockReturnThis()
+        write : jest.fn().mockReturnThis(),
+        toJson : jest.fn().mockReturnThis()
     };
 
     const repository = new JetpackRepository(dbMock);
-    test('Create jetpack', () => {
-        repository.create(new Jetpack());
+    test('Create jetpack fail', () => {
+        jp = {};
+        
+        try {
+            repository.create(jp);
+            // Fail test if above expression doesn't throw anything.
+            expect(true).toBe(false);
+        } catch (e) {
+            expect(e).toBe("Jetpack object is missing information");
+        }
+    });
+
+    test('Create jetpack OK', () => {
+        jp = {id : '1',
+        name: 'X1982BD',
+        image: 'base64...',
+        toJson() { jest.fn().mockReturnThis() }
+    };
+        
+        repository.create(jp);
         expect(dbMock.write.mock.calls.length).toBe(1);
     });
 });
@@ -18,15 +36,13 @@ describe('JetpackCreation', function() {
 describe('JetpackRepository list', function () {
     let jetpacks = []
 
-    let jetpack1 = new Jetpack();
-    jetpack1.id = "1";
-    jetpack1.name = "X1982BD";
-    jetpack1.image = "base64...";
+    let jetpack1 = {id : '1',
+    name: 'X1982BD',
+    image: 'base64...'};
 
-    let jetpack2 = new Jetpack();
-    jetpack2.id = "2";
-    jetpack2.name = "Lol";
-    jetpack2.image = "base64...";
+    let jetpack2 = {id : '2',
+    name: 'Lol',
+    image: 'base64...'};
 
     jetpacks.push(jetpack1);
     jetpacks.push(jetpack2);
